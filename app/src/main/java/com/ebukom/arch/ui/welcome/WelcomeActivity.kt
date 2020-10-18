722a756b3f500c70f5f1f55b862a651becc322c7
novitaguok <novitaguok@gmail.com>
2020-10-18 21:23:29 +0700

    Welcome Page Layout done

---
+++ app/src/main/java/com/ebukom/arch/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.ebukom.arch.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ebukom.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
